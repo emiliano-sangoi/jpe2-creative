661cdde1ee3267782bd43864a9c2e77520c32878
Emiliano <emiliano.sangoi@gmail.com>
2017-08-16 10:54:49 -0300

    Primer commit. Archivos iniciales del tema y carga de scripts y estilos con wp_enqueue_*

---
+++ functions.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+* functions.php
+*
+* 
+*
+*
+*/
+
+
+
+
+
+function jpe2_set_estilos(){
+
+	//Boostrap styles	
+	wp_enqueue_style("bootstrap-css-core", get_template_directory_uri() . "/node_modules/bootstrap/dist/css/bootstrap.min.css");
+
+	//Font Awesome	
+	wp_enqueue_style("font-awesome", get_template_directory_uri() . "/node_modules/font-awesome/css/font-awesome.min.css");
+
+	//Plugin CSS:
+	wp_enqueue_style("magnific-popup-css", get_template_directory_uri() . "/node_modules/magnific-popup/dist/magnific-popup.css");
+
+	//Creative curstom css:
+	wp_enqueue_style("creative-css", get_template_directory_uri() . "/css/creative/creative.min.css");
+
+	//Custom Style:
+	wp_enqueue_style("jpe2-style", get_template_directory_uri() . "/style.css");
+
+}
+add_action('wp_head', 'jpe2_set_estilos');
+
+function jpe2_set_scripts(){
+	
+	//JQuery:
+	wp_enqueue_script("jquery", get_template_directory_uri() . "/node_modules/jquery/dist/jquery.min.js", array(), false, true);	
+	
+        //Popper:
+	wp_enqueue_script("popper-js", get_template_directory_uri() . "/node_modules/popper.js/dist/popper.min.js", array('jquery'), false, true);
+        
+	//Bootstrap:
+	wp_enqueue_script("bootstrap-js-core", get_template_directory_uri() . "/node_modules/bootstrap/dist/js/bootstrap.min.js", array('jquery', 'popper-js'), false, true);			
+
+	// ---- Plugin JavaScript  ----
+   
+	//JQuery Easing:
+	wp_enqueue_script("jquery-easing", get_template_directory_uri() . "/node_modules/jquery.easing/jquery.easing.min.js", false, true);
+
+	//Scroll Reveal:
+	wp_enqueue_script("scroll-reveal", get_template_directory_uri() . "/node_modules/scrollreveal/dist/scrollreveal.min.js", false, true);
+
+	//Magnific Popup
+	wp_enqueue_script("magnific-popup", get_template_directory_uri() . "/node_modules/magnific-popup/dist/jquery.magnific-popup.min.js", false, true);
+
+	//Creative custom js:
+	wp_enqueue_script("creative-js", get_template_directory_uri() . "/js/creative/creative.min.js", false, true);	
+
+
+
+}
+add_action('wp_footer', 'jpe2_set_scripts');
---
+++ header.php
@@ -0,0 +1,61 @@
+<?php 
+
+    ob_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?php bloginfo('name'); ?></title>
+
+    <!-- Custom fonts for this template -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+
+<?php wp_head(); ?>
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#portfolio">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <header class="masthead">
+      <div class="header-content">
+        <div class="header-content-inner">
+          <h1 id="homeHeading">Your Favorite Source of Free Bootstrap Themes</h1>
+          <hr>
+          <p>Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
+          <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
+        </div>
+      </div>
+    </header>
